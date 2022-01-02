b191650dd99917be5f88d1ebb07a8933beb548e5
Roudan Chirkoh <roudan.chirkoh@gmail.com>
2022-01-02 15:51:18 +0300

    API call

--- src/components/Forcast/Forcast.jsx
+++ src/components/Forcast/Forcast.jsx
@@ -1,8 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./Forcast.css";
 
-function Details(props) {
+function Forcast(props) {
+  const getWeatherinfo = () => {
+    const weatherData = fetch(
+      `http://api.weatherapi.com/v1/current.json?key=${process.env.REACT_APP_API_KEY}&q=Berlin&aqi=no`
+    )
+      .then(res => res.json())
+      .then(res => console.log(res));
+  };
+
+  useEffect(() => {
+    getWeatherinfo();
+  });
   return <div>Hello world</div>;
 }
 
-export default Details;
+export default Forcast;
