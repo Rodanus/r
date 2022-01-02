2d43eacc817054da2e44d4dd4cdeb239715bbb86
Roudan Chirkoh <roudan.chirkoh@gmail.com>
2022-01-02 15:22:32 +0300

    Add cities

--- src/components/Homepage/Homepage.jsx
+++ src/components/Homepage/Homepage.jsx
@@ -3,6 +3,12 @@ import Cities from "../Cities/Cities";
 import "./Homepage.css";
 
 function Homepage(props) {
+  const citiesToDisplay = [
+    { name: "Berlin" },
+    { name: "London" },
+    { name: "New York" }
+  ];
+
   return (
     <div className="homepage-container">
       <Cities listOfCities={citiesToDisplay} />
