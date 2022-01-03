ecda56d601dfad45b49e37000e25a0f48409a5e8
Roudan Chirkoh <roudan.chirkoh@gmail.com>
2022-01-03 05:46:20 +0300

    Display data from API

--- src/components/Forcast/Forcast.jsx
+++ src/components/Forcast/Forcast.jsx
@@ -1,14 +1,27 @@
-import React, { useEffect } from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
 import "./Forcast.css";
 
 function Forcast(props) {
+  const { cityName } = useParams();
+  const [cityForecast, setCityForecast] = useState({
+    cityName: "",
+    temp: 0,
+    icon: ""
+  });
+
   const getWeatherinfo = () => {
     const weatherData = fetch(
-      `http://api.weatherapi.com/v1/current.json?key=${process.env.REACT_APP_API_KEY}&q=Berlin&aqi=no`
+      `http://api.weatherapi.com/v1/current.json?key=${process.env.REACT_APP_API_KEY}&q=${cityName}&aqi=no`
     )
       .then(res => res.json())
-      .then(res => console.log(res));
+      .then(res =>
+        setCityForecast({
+          cityName: res.location.name,
+          temp: res.current.temp_c,
+          icon: res.current.condition.icon
+        })
+      );
   };
 
   useEffect(() => {
@@ -17,9 +30,20 @@ function Forcast(props) {
   return (
     <div>
       <Link to="/" className="homepage-link">
-        Home
+        {"<"} Back To Home
       </Link>
-      <p>Hello world</p>
+      <div className="forecast-details-container">
+        <h2 className="forecast-city-name">{cityForecast.cityName}</h2>
+        <div className="forecast-details">
+          <img
+            src={cityForecast.icon}
+            alt="weather status icon"
+            className="weather-icon"
+          ></img>
+          <span className="temp">{cityForecast.temp}°</span>
+          <span className="celsius">C</span>
+        </div>
+      </div>
     </div>
   );
 }
